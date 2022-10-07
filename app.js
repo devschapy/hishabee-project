07464ebb90f7cacf8659810e6861d3da49f02b17
devschapy <devschapy71@gmail.com>
2022-10-07 21:57:56 +0600

    Complete Project

---
+++ app.js
@@ -0,0 +1,19 @@
+const checkbox = document.querySelector('#id1')
+const id1Price = document.querySelector('.id1-price')
+const totalPayment = document.querySelector('.total-payment')
+
+checkbox.addEventListener('change', (e)=>{
+    const prevPrice =totalPayment.innerHTML
+    if(e.target.checked){
+        localStorage.setItem('prevPrice', prevPrice)
+        const price = id1Price.innerHTML.slice(1)
+        totalPayment.innerHTML = price
+    }else{
+        const getPrevPrice = localStorage.getItem('prevPrice')
+        totalPayment.innerHTML = getPrevPrice
+        console.log(prevPrice);
+    }
+})
+
+
+
